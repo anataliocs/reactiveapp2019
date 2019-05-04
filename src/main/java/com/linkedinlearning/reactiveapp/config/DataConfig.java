03c6722d152ddb54194597cafb1c4dcbff6d217c
chrisanatalio <anataliocs@gmail.com>
2019-05-04 10:59:57 -0700

    Define mongo template bean

--- src/main/java/com/linkedinlearning/reactiveapp/config/DataConfig.java
+++ src/main/java/com/linkedinlearning/reactiveapp/config/DataConfig.java
@@ -1,9 +1,14 @@
 package com.linkedinlearning.reactiveapp.config;
 
 
+import com.mongodb.reactivestreams.client.MongoClient;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @EnableReactiveMongoRepositories
@@ -11,6 +16,15 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 @Import(EmbeddedMongoAutoConfiguration.class)
 public class DataConfig {
 
+    private static final String DATABASE_NAME = "reservations";
 
+    @Bean
+    public ReactiveMongoDatabaseFactory mongoDbFactory(MongoClient mongoClient) {
+        return new SimpleReactiveMongoDatabaseFactory(mongoClient, DATABASE_NAME);
+    }
 
+    @Bean
+    public ReactiveMongoTemplate reactiveMongoTemplate(ReactiveMongoDatabaseFactory mongoDbFactory) {
+        return new ReactiveMongoTemplate(mongoDbFactory);
+    }
 }
--- src/main/java/com/linkedinlearning/reactiveapp/model/Reservation.java
+++ src/main/java/com/linkedinlearning/reactiveapp/model/Reservation.java
@@ -2,7 +2,6 @@ package com.linkedinlearning.reactiveapp.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
@@ -10,15 +9,15 @@ import java.time.LocalDate;
 @Document
 public class Reservation {
 
-    private final Long roomId;
+    private final Long roomNumber;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private final LocalDate checkin;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private final LocalDate checkout;
     private final Integer price;
 
-    public Reservation(Long roomId, LocalDate checkin, LocalDate checkout, Integer price) {
-        this.roomId = roomId;
+    public Reservation(Long roomNumber, LocalDate checkin, LocalDate checkout, Integer price) {
+        this.roomNumber = roomNumber;
         this.checkin = checkin;
         this.checkout = checkout;
         this.price = price;
@@ -27,8 +26,8 @@ public class Reservation {
     @Id
     private String id;
 
-    public Long getRoomId() {
-        return roomId;
+    public Long getRoomNumber() {
+        return roomNumber;
     }
 
     public LocalDate getCheckin() {
